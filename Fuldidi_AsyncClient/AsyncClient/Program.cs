9e59447155ecaa188d3b1dd7a65c528864b022bd
iurifulgidi <iurifulgidi@hotmail.com>
2021-02-05 11:05:51 +0100

    aggiunta connessione al server, ricezione messaggi ed invio messaggi

--- Fuldidi_AsyncClient/AsyncClient/Program.cs
+++ Fuldidi_AsyncClient/AsyncClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SocketAsync;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,33 @@ namespace AsyncClient
     {
         static void Main(string[] args)
         {
+            string ip = null;
+            string port= null;
+            bool riprova = true;
+            AsyncSocketClient mClient = new AsyncSocketClient();
+            while (riprova)
+            {
+                Console.Write("Inserisci IP:");
+                ip = Console.ReadLine();
+                Console.Write("Inserisci Porta:");
+                port = Console.ReadLine();
+                
+                if (mClient.SetServerIPAddress(ip) && mClient.SetServerPort(port))
+                {
+                    mClient.ConnectToServer();
+                    riprova = false;
+                }
+            }
+
+            while(true)
+            {
+                string msg = Console.ReadLine().ToLower();
+                if (msg == "quit")
+                    break;
+
+                mClient.SendMessage(msg);
+            }
+
         }
     }
 }
